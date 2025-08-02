ab1b85eb20de7f73f5e68147d4a7a5e882145bc2
Shannon Bray <shannon.bray@parextechnology>
2025-08-02 07:37:40 -0400

    Initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";  // Corrected path
+import "../index.css";    // Assuming this is in the root
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
